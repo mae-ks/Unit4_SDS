c052da410cbbb634fae4b5a985e5070775044868
Mae <mae.kyawsan@bison.howard.edu>
2022-04-16 20:36:23 -0400

    Heroku

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import os
 
 # Run line below in terminal
 # export MONGO_URI="mongodb+srv://admin:15112001@cluster0.v0lkc.mongodb.net/Unit4?retryWrites=true&w=majority"
+# https://medium.com/@gitaumoses4/deploying-a-flask-application-on-heroku-e509e5c76524
 
 # -- Initialization --
 app = Flask(__name__)
